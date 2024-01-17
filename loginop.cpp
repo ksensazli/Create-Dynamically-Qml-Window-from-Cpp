f72316f41b1dc03cc88036f788503a3e7c079844
Kağan <kagansensazli@gmail.com>
2024-01-17 22:54:13 +0300

    bug-fix file read method

--- loginop.cpp
+++ loginop.cpp
@@ -2,16 +2,17 @@
 
 loginOp::loginOp(QObject *parent) : QObject{parent}
 {
-    QFile file("/Users/ksensazli/Qt_Projects/loginSystem/database.csv");
-    file.open(QIODevice::ReadOnly);
-    QTextStream stream(&file);
-    userList = stream.readAll().split(QRegExp("[\r\n';']"), QString::SkipEmptyParts);
-    qDebug() << userList;
-    file.close();
+
 }
 
 void loginOp::userCheck(const QString &username, const QString &password)
 {
+    QFile file("/Users/ksensazli/Qt_Projects/loginSystem/database.csv");
+    file.open(QIODevice::ReadOnly);
+    QTextStream stream(&file);
+    userList = stream.readAll().split(QRegExp("[\r\n';']"), QString::SkipEmptyParts);
+    file.close();
+
     for(int i=0; i<userList.length(); i+=2)
     {
         if(userList.at(i) == username && userList.at(i+1) == password)
