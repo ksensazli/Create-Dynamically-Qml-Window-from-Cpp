8d1705e4ba075b471bb2b00dbbed605633b99ee7
Kağan <kagansensazli@gmail.com>
2024-01-17 16:16:06 +0300

    Update loginop.h

--- loginop.h
+++ loginop.h
@@ -19,10 +19,17 @@ public:
     explicit loginOp(QObject *parent = nullptr);
 
     Q_INVOKABLE void userCheck(const QString &username, const QString &password);
-    Q_PROPERTY(QString handledUser READ handledUserText WRITE handledUserSet NOTIFY handledUserChanged)
+    Q_PROPERTY(QString handledUser READ handledUserText WRITE handledUserSet NOTIFY handledUserChanged);
+    Q_PROPERTY(bool correction READ correctionText WRITE correctionSet NOTIFY correctionChanged);
+
+    bool correction;
 
     QString handledUserText() const{
         return handledUser;
+    }
+
+    bool correctionText() const{
+        return correction;
     }
 
     void handledUserSet(const QString &handledUserText){
@@ -32,12 +39,20 @@ public:
         emit handledUserChanged(handledUser);
     }
 
+    void correctionSet(const bool &correctionText){
+        if(correctionText == correction)
+            return;
+        correction = correctionText;
+        emit correctionChanged(correction);
+    }
+
 private:
     QStringList userList;
     QString handledUser;
 
 signals:
     void handledUserChanged(const QString & handledUser);
+    void correctionChanged(const bool & correction);
 };
 
 #endif // LOGINOP_H
